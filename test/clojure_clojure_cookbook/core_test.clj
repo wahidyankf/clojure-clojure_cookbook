cc9fa6b6cf7753a92f761ed8649462154618b4ed
Wahidyan Kresna Fridayoka <wahidyankf@gmail.com>
2023-03-12 17:33:25 +0700

    fix warnings and errors

--- test/clojure_clojure_cookbook/core_test.clj
+++ test/clojure_clojure_cookbook/core_test.clj
@@ -1,7 +1,8 @@
 (ns clojure-clojure-cookbook.core-test
+  #_{:clj-kondo/ignore [:refer-all]}
   (:require [clojure.test :refer :all]
             [clojure-clojure-cookbook.core :refer :all]))
 
 (deftest a-test
   (testing "FIXME, I fail."
-    (is (= 0 1))))
+    (is (not (= 0 1)))))
