0152a435c6551f12eef82fdff6dabc1ebf3acbd5
Wahidyan Kresna Fridayoka <wahidyankf@gmail.com>
2023-03-12 17:30:27 +0700

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clojure-clojure_cookbook
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/clojure_clojure_cookbook/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-clojure-cookbook.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-clojure-cookbook.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
