a8fc459608553a2f57ea9d5e4fb91f30c98e24aa
Wahidyan Kresna Fridayoka <wahidyankf@gmail.com>
2023-03-17 16:42:58 +0700

    finish jdbc + honey sql test

--- src/ch06_databases.clj
+++ src/ch06_databases.clj
@@ -1,14 +1,29 @@
 (ns ch06-databases
-  (:require [next.jdbc :as jdbc]))
+  (:require
+   [honey.sql :as sql]
+   [next.jdbc.sql :refer [query]]
+   [next.jdbc.result-set :as rs]))
 
 (def ds {:dbtype "postgres"
          :dbname "postgres" :user "postgres" :password "mysecretpassword"})
 
-(jdbc/execute! ds ["
+(query ds ["
 create table address (
   id SERIAL PRIMARY KEY,
   name VARCHAR(32),
   email VARCHAR(255)
 )"])
 
-(jdbc/execute-one! ds ["select * from accounts"])
+(query ds ["
+insert into address(name,email)
+  values('Sean Corfield','sean@corfield.org')"])
+
+(query ds ["select * from address"])
+(query ds ["delete from address where id = 3"])
+
+(def res (query ds (sql/format {:select [:*],
+                                :from [:address]})
+                {:return-keys true
+                 :builder-fn rs/as-unqualified-maps}))
+
+(:id (first res))
